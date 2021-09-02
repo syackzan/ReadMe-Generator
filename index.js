2e5316cd406c06050056c564541803c6a0b0f567
Scotty Yackzan <syackzan@gmail.com>
2021-09-02 09:49:27 -0700

    Added all prompt questions to store user Info

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const fs = require("fs");
 //const generateMarkdown = require("generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "Enter the Title of the project:",
+    "Enter a short Description of the project:",
+    "Enter Installation Instructions:",
+    "Enter Usage Information:",
+    "Enter Contribution Guidelines:",
+    "Enter Test Instructions:",
+    "What License will you use:",
+    "Enter your Github Account",
+    "Enter Your Email for contact purposes:"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
+    fs.writeFile(fileName, JSON.stringify(data, null, "\t"), (err) =>
     err ? console.error(err) : console.log('Success!')
 );
 }
@@ -19,25 +29,53 @@ function init() {
   .prompt([
     {
       type: 'input',
-      message: 'What is your user name?',
-      name: 'username',
+      message: questions[0],
+      name: 'Title',
     },
     {
-      type: 'password',
-      message: 'What is your password?',
-      name: 'password',
+    type: 'input',
+    message: questions[1],
+    name: 'Description',
     },
     {
-      type: 'password',
-      message: 'Re-enter password to confirm:',
-      name: 'confirm',
+    type: 'input',
+    message: questions[2],
+    name: 'Installlation',
+    },
+    {
+    type: 'input',
+    message: questions[3],
+    name: 'Usage Info',
+    },
+    {
+    type: 'input',
+    message: questions[4],
+    name: 'Constribution Guidelines',
+    },
+    {
+    type: 'input',
+    message: questions[5],
+    name: 'Test Instru',
+    },
+    {
+    type: 'list',
+    choices: ["MIT", "WXYZ", "ZTBS"],
+    message: questions[6],
+    name: 'License'
+    },
+    {
+    type: 'input',
+    message: questions[7],
+    name: 'Github:',
+    },
+    {
+    type: 'input',
+    message: questions[8],
+    name: 'Email:',
     },
   ])
   .then((response) => {
-    response.confirm === response.password
-      ? console.log('Success!')
-      : console.log('You forgot your password already?!')
-      writeToFile("test.md", response);
+    writeToFile("test.md", response);
   });
 }
 
---
+++ test.md
@@ -0,0 +1,11 @@
+{
+	"Title": "Title",
+	"Description": "Description",
+	"Installlation": "Installation",
+	"Usage Info": "Usage",
+	"Constribution Guidelines": "Contribution",
+	"Test Instru": "Test",
+	"License": "WXYZ",
+	"Github:": "syackzan",
+	"Email:": "syackzan@gmail.com"
+}
